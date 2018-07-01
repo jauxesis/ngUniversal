cdcf9c51d15b0e3b7b7b16fd353fd2a4207b185e
Jitendra Patwa <jitendrapatwa135@gmail.com>
2018-07-01 14:48:43 +0530

    ad

---
+++ src/app/mypage/mypage.component.ts
@@ -0,0 +1,39 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
+
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import 'rxjs/add/operator/switchMap'; //to fetch url params
+
+@Component({
+  selector: 'app-mypage',
+  templateUrl: './mypage.component.html',
+  styleUrls: ['./mypage.component.scss']
+})
+export class MypageComponent implements OnInit {
+
+  pageIdentifier:string = '';
+  pageUrl:string = '';
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,@Inject(DOCUMENT) private document: any,public http:HttpClient) { }
+
+  ngOnInit() {
+    
+    
+    let iid = this.route.snapshot.paramMap.get("id");
+    if(iid == null || iid == ""){
+      this.pageIdentifier = 'home';
+      this.pageUrl = this.document.location.href;
+      console.log("none",this.pageIdentifier,this.pageUrl);
+    }else{
+      this.route.params.subscribe(params => {
+          let id = params['id'];
+          this.pageIdentifier = id;
+          this.pageUrl = this.document.location.href;
+          console.log("isid",this.pageIdentifier,this.pageUrl);
+      });
+    }
+  }
+
+}
